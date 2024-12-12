5d779938f565fb5517b64d687091cc62c72ded05
KGandhi90 <kevin.gandhi@somaiya.edu>
2024-12-12 16:30:38 +0530

    Law facts file added

--- public/js/script.js
+++ public/js/script.js
@@ -3,15 +3,16 @@ let menuBar = document.getElementById("menubar");
 let menuBarContainer = document.getElementById("menubar-container");
 
 menuIcon.addEventListener("click", () => {
-    if(menubar.classList.contains("hidden")){
-        menubar.classList.remove("hidden", "w-0");
-        menuBarContainer.remove("hidden");
-        menubar.classList.add("sm:w-[40vw]", "w-[100vw]", "visible");
-        menuBarContainer.add("visible");
+    if(menuBar.classList.contains("hidden")){
+        menuBar.classList.toggle
+        ("hidden", "w-0");
+        menuBarContainer.classList.remove("hidden");
+        menuBar.classList.add("sm:w-[40vw]", "w-[100vw]", "visible");
+        menuBarContainer.classList.add("visible");
     } else {
-        menubar.classList.remove("sm:w-[40vh]", "w-[100vw]", "visible");
-        menuBarContainer.remove("visible");
-        menubar.classList.add("hidden", "w-0");
-        menuBarContainer.add("hidden");
-    }
+        menuBar.classList.remove("sm:w-[40vh]", "w-[100vw]", "visible");
+        menuBarContainer.classList.remove("visible");
+        menuBar.classList.add("hidden", "w-0");
+        menuBarContainer.classList.add("hidden");
+    };
 });
